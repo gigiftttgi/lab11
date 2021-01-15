20926747ee9dca60c4aa0d5689eac1be30cd7b0d
gigiftttgi <natcha_si@cmu.ac.th>
2021-01-15 15:11:31 +0700

    lab11_1_fianl

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int fibonacci(int);
+long long int fibonacci(int);
 
 int main(){
     int x;
@@ -9,7 +9,7 @@ int main(){
     return 0; 
 }
 
-int fibonacci(int x)
+long long int fibonacci(int x)
 {
 	if(x==0)
 		{return 0;}
